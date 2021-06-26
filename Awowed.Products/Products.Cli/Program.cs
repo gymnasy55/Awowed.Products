1db780480152a2e5ca6cd6052785a5ed0f666dbc
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-06-26 14:29:38 +0300

    Added some checking

--- Awowed.Products/Products.Cli/Program.cs
+++ Awowed.Products/Products.Cli/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Products.Domain.Models;
+﻿using Products.Domain.Models;
 using Products.Domain.Repositories;
 using Products.Service.Services;
 
@@ -13,26 +10,66 @@ namespace Products.Cli
         {
             var fileWorker = (IFileWorker) new FileWorker();
             var jsonWorker = (IJsonWorker) new JsonWorker();
-            var fileName = "info.json";
-            
-            var repos = new ProductsRepository(fileWorker, jsonWorker);
-            
-            // repos.SaveProduct(new Product());
-            // repos.SaveProduct(new Product());
-            // repos.SaveProduct(new Product());
-            // repos.SaveProduct(new Product());
-            // repos.SaveProduct(new Product());
-            //
-            // repos.SaveToFile(fileName);
+            var prodFileName = "products.json";
+            var ordersFileName = "orders.json";
+            var usersFileName = "users.json";
 
-            repos.LoadFromFile(fileName);
+            #region Products
+
+            // var prodRepos = new ProductsRepository(fileWorker, jsonWorker);
+            //
+            // prodRepos.Save(new Product());
+            // prodRepos.Save(new Product());
+            // prodRepos.Save(new Product());
+            // prodRepos.Save(new Product());
+            // prodRepos.Save(new Product());
+            //
+            // prodRepos.SaveToFile(prodFileName);
+            //
+            // prodRepos.LoadFromFile(prodFileName);
+            //
+            // var products = prodRepos.GetAll().Values;
+            //
+            // foreach (var product in products)
+            // {
+            //     Console.WriteLine(product);
+            // }
+
+            #endregion
+
+            #region Orders
+
+            //var ordersRepos = new OrdersRepository(fileWorker, jsonWorker);
+            //
+            // ordersRepos.Save(new Order());
+            // ordersRepos.Save(new Order());
+            // ordersRepos.Save(new Order());
+            // ordersRepos.Save(new Order());
+            // ordersRepos.Save(new Order());
+            // ordersRepos.SaveToFile(ordersFileName);
+            //
+            // ordersRepos.LoadFromFile(ordersFileName);
+            //
+            // var orders = ordersRepos.GetAll().Values;
+            // foreach (var order in orders)
+            // {
+            //     Console.WriteLine(order);
+            // }
+
+            #endregion
+
+            #region Users
+
+            var usersRepos = new UsersRepository(fileWorker, jsonWorker);
             
-            var products = repos.GetAllProducts().Values;
+            usersRepos.Save(new User());
+            usersRepos.Save(new User());
+            usersRepos.Save(new User());
+            usersRepos.Save(new User());
+            usersRepos.Save(new User());
             
-            foreach (var product in products)
-            {
-                Console.WriteLine(product);
-            }
+            usersRepos.SaveToFile(usersFileName);
+            #endregion
         }
     }
 }
